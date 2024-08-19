7d552458f8074a3a447fd91037a5c59c1721ad65
Nguyen Trung Kien <nguyentrungkien17102004@gmail.com>
2024-08-19 22:46:53 +0700

    add files store,  Banner.js and MobileNavigation.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MobileNavigation from "./components/MobileNavigation";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setBannerData, setImageURL } from "./store/movieoSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const fetchTrendingData = async () => {
+    try {
+      const respond = await axios.get("trending/all/week");
+
+      dispatch(setBannerData(respond.data.results));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const fetchConfiguration = async () => {
+    try {
+      const response = await axios.get("/configuration");
+
+      dispatch(setImageURL(response.data.images.secure_base_url + "original"));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrendingData();
+    fetchConfiguration();
+  }, []);
+
   return (
     <main className="pb-14 lg:pb-0">
       <Header />
-      <div className="pt-16">
+      <div>
         <Outlet />
       </div>
       <Footer />
